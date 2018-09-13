7baf8d23b8f70d62ae9a244ddd3ba58f3950831e
Anmol Desai <anmoldesai4@gmail.com>
2018-09-13 23:06:13 +1000

    basic front end backend

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const routes = require('./routes/api');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise; // overriding the deprecaated promise in mongo db
+
+app.use(express.static('./public'));
 
 // middleware runs between req and res
 app.use(bodyParser.json());
 
 // anyting after /api uses the routes file
 app.use('/api', routes); 
+
+// error handling middleware. We write our own here. This middle ware is hit if next is called in the routes.
+app.use((err, req, res, next) => {
+    res.status(422).send({error: err.message});
+});
 
 app.listen(process.env.port || 4000, () => {
     console.log("Stared Server");
--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const Ninja = require('../models/ninja');
 
 // get a list of ninjas from the database
-router.get('/ninjas', (req, res) => {
-    res.send({type: 'GET'});
+router.get('/ninjas', (req, res, next) => {
+    // Ninja.find({}).then((ninjas) => {
+    //     res.send(ninjas);
+    // });
+    Ninja.aggregate().near({
+        near: [parseFloat(req.query.lng), parseFloat(req.query.lat)],
+        maxDistance: 100000,
+        spherical: true,
+        distanceField: "dist.calculated"
+    }).then(function(ninjas){
+        res.send(ninjas);
+    });;
 });
 
 // add a new ninja to the database
-router.post('/ninjas', (req, res) => {
-    res.send({
-        type: 'POST',
-        name: req.body.name,
-        rank: req.body.rank
-    });
+router.post('/ninjas', (req, res, next) => {
+    // create a new ninja record
+    // creates new ninja object and then save it automatically.
+    Ninja.create(req.body).then((ninja) => {
+        res.send(ninja);
+    }).catch(next); // goes to the next piece of middleware
 });
 
 // edit a ninja in the database
-router.put('/ninjas/:id', (req, res) => {
-    res.send({type: 'PUT'});
+router.put('/ninjas/:id', (req, res, next) => {
+    Ninja.findByIdAndUpdate({_id: req.params.id}, req.body).then(() => {
+        Ninja.findOne({_id: req.params.id}).then((ninja) => {
+            res.send(ninja);
+        });
+    });
 });
 
 // detele a ninja in the database
-router.delete('/ninjas/:id', (req, res) => {
-    res.send({type: 'DELETE'});
+router.delete('/ninjas/:id', (req, res, next) => {
+    Ninja.findByIdAndRemove({_id: req.params.id}).then((ninja) => {
+        res.send(ninja);
+    });
 });
 
 module.exports = router;
